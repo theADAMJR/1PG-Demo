f284ce2d78de175c115ce4b3293bda78b6b5902d
theADAMJR <adamjf123@gmail.com>
2020-09-10 09:29:13 +0100

    add something cool

---
+++ bot.js
@@ -0,0 +1,6 @@
+const { Client } = require('discord.js');
+const bot = new Client();
+
+bot.on('ready', () => console.log('Ready!'));
+
+bot.login('NzQwNDkyODIwNTA2MzQ1NTE0.Xypzrg.iI1ye1zpzKmJt2WEp28bUkoedkw');
